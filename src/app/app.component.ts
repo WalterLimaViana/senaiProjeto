8ce4c2974d1afc003d904978cf7e70a74635dad2
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-06 15:37:24 -0300

    incorporando um DB ao projeto através do HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-root",
@@ -6,12 +7,22 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  cursos = [
-    { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
-    { url: "./../assets/img/online-cursos2.jpg", description: "Eletrônica" },
-    { url: "./../assets/img/online-cursos.jpg", description: "Full Stack" },
-    { url: "./../assets/img/online-cursos2.jpg", description: "Eletrica" },
-    { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
-    { url: "./../assets/img/online-cursos2.jpg", description: "Back End" },
-  ];
+  // cursos = [
+  //   { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
+  //   { url: "./../assets/img/online-cursos2.jpg", description: "Eletrônica" },
+  //   { url: "./../assets/img/online-cursos.jpg", description: "Full Stack" },
+  //   { url: "./../assets/img/online-cursos2.jpg", description: "Eletrica" },
+  //   { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
+  //   { url: "./../assets/img/online-cursos2.jpg", description: "Back End" },
+  // ];
+
+  cursos: Object[] = [];
+
+  constructor(http: HttpClient) {
+    http
+      .get<Object[]>(`http://localhost:3000/flavio/photos`)
+      .subscribe((photos) => {
+        this.cursos = photos;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from "./app.component";
 import { AlunosModule } from "./alunos/alunos.module";
 import { CursoComponent } from "./cursos/curso/curso.component";
 import { CursosModule } from "./cursos/cursos.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AlunosModule, CursosModule],
+  imports: [BrowserModule, AlunosModule, CursosModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })

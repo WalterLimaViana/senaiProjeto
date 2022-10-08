a6764d19692b563831cd3a711b515ad79f4b61e6
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-07 22:49:23 -0300

    alterando o menu principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,22 @@ import { HttpClient } from "@angular/common/http";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  // cursos = [
-  //   { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
-  //   { url: "./../assets/img/online-cursos2.jpg", description: "Eletrônica" },
-  //   { url: "./../assets/img/online-cursos.jpg", description: "Full Stack" },
-  //   { url: "./../assets/img/online-cursos2.jpg", description: "Eletrica" },
-  //   { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
-  //   { url: "./../assets/img/online-cursos2.jpg", description: "Back End" },
-  // ];
+  cursos = [
+    { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
+    { url: "./../assets/img/online-cursos2.jpg", description: "Eletrônica" },
+    { url: "./../assets/img/online-cursos.jpg", description: "Full Stack" },
+    { url: "./../assets/img/online-cursos2.jpg", description: "Eletrica" },
+    { url: "./../assets/img/online-cursos.jpg", description: "Administração" },
+    { url: "./../assets/img/online-cursos2.jpg", description: "Back End" },
+  ];
 
-  cursos: Object[] = [];
+  // cursos: Object[] = [];
 
-  constructor(http: HttpClient) {
-    http
-      .get<Object[]>(`http://localhost:3000/flavio/photos`)
-      .subscribe((photos) => {
-        this.cursos = photos;
-      });
-  }
+  // constructor(http: HttpClient) {
+  //   http
+  //     .get<Object[]>(`http://localhost:3000/flavio/photos`)
+  //     .subscribe((photos) => {
+  //       this.cursos = photos;
+  //     });
+  // }
 }

124317c004fada8bde798a7b0b02eadf198b614b
WalterLimaViana <walter.lima.viana@gmail.com>
2022-10-08 22:41:44 -0300

    criando o header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AlunosModule } from "./alunos/alunos.module";
 import { CursoComponent } from "./cursos/curso/curso.component";
 import { CursosModule } from "./cursos/cursos.module";
 import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AlunosModule, CursosModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
